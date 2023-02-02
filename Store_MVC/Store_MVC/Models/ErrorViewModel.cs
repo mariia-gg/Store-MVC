f2f404b2abafd5df9b65738177791cf7819974d5
mariia-gg <72667038+mariia-gg@users.noreply.github.com>
2023-02-02 16:07:39 +0200

    Delete Store_MVC directory

--- Store_MVC/Store_MVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Store_MVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
